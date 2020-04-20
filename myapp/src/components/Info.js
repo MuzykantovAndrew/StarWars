b52530e6291cfb328a811ee59ac73b680d1554bc
YuukoKanoe <musicantov@gmail.com>
2020-04-20 15:12:23 +0300

    Task1

---
+++ myapp/src/components/Info.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+function Info() {
+return(
+    <div>
+    <h1>Andrew</h1>
+    <p>Im from Donetsk. I love Fortnite</p>
+    <ul>
+        <li>Portugal</li>
+        <li>Spain</li>
+        <li>England</li>
+    </ul>
+    </div>
+)
+} 
+
+export default Info;
+
